4976e6189d73a981728ec02dc55dc92cb54af288
Robdetta <robdetta@gmail.com>
2023-08-27 16:31:51 -0400

    updates to backend

--- backend/src/rooms/CardGameRoom.ts
+++ backend/src/rooms/CardGameRoom.ts
@@ -1,43 +1,28 @@
-import { Room, Client } from 'colyseus';
+import { Room, Client } from '@colyseus/core';
+import { MyRoomState } from './schema/MyRoomState';
 
-export class CardGameRoom extends Room {
-  // State definition
-  state: {
-    players: {
-      [id: string]: {
-        name: string;
-        hand: any[];
-      };
-    };
-  };
+export class CardGameRoom extends Room<MyRoomState> {
+  maxClients = 4;
 
-  // This is called when the room is initialized.
   onCreate(options: any) {
-    this.setState({
-      players: {},
+    this.setState(new MyRoomState());
+
+    this.onMessage('type', (client, message) => {
+      //
+      // handle "type" message
+      //
     });
   }
 
-  // When a client joins the room
   onJoin(client: Client, options: any) {
-    this.state.players[client.sessionId] = {
-      name: options.name || 'Anonymous',
-      hand: [], // Initially empty hand, can be filled as per game logic
-    };
+    console.log(client.sessionId, 'joined!');
   }
 
-  // When a client sends a message
-  onMessage(client: Client, message: any) {
-    // Handle game-related messages here, like moving a card, playing a card, etc.
-  }
-
-  // When a client leaves the room
   onLeave(client: Client, consented: boolean) {
-    delete this.state.players[client.sessionId];
+    console.log(client.sessionId, 'left!');
   }
 
-  // Cleanup callback for the room. Can be used for custom logic.
   onDispose() {
-    // Cleanup logic, if needed
+    console.log('room', this.roomId, 'disposing...');
   }
 }
